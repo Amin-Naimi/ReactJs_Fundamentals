a4737957ea8a24b81d0be7f133a09a5e984bad69
Mohamed <ahnbi32@gmail.com>
2024-02-04 13:44:43 +0100

    first functional components

---
+++ hello_world/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/*function Greet(){
+    return <h1>Hello Mohamed</h1>
+}*/
+// named export
+export const Greet = () => <h1>Hello Mohamed</h1>
+//export default Greet;
