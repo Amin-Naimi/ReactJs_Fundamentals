f8e512e9a5c78e4ecd5cfe611dbef03450b0cf68
Mohamed <ahnbi32@gmail.com>
2024-02-10 22:25:48 +0100

    Event Handling

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -5,29 +5,27 @@ import { Greet } from './components/Greet';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Malika" lastName="Jali"></Greet>
-      <Hello name="Mohamed" lastName="Naimi"></Hello>
-
-
-
-
+      <ClassClick></ClassClick>
 
       {
-      /*
-            <Counter></Counter>
 
+      /*
+      <FunctionClick></FunctionClick>
+      <Greet name="Malika" lastName="Jali"></Greet>
+      <Hello name="Mohamed" lastName="Naimi"></Hello>
+            <Counter></Counter>
+      <Message></Message>      
       <Greet name="Mohamed" lastName="Naimi">
-        <p>This is children props</p>
-      </Greet>
-      
+        <p>This is children props</p></Greet>
       <Greet name="Redtha" lastName="Naimi"></Greet>
-      
-  <Hello name="Malika" lastName="Jali"></Hello>
-  <Message></Message>*/}
+      <Hello name="Malika" lastName="Jali"></Hello>
+  */}
 
 
     </div>
---
+++ hello_world/src/components/ClassClick.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ClassClick extends Component {
+    
+    clickHandler(){
+        console.log(" Clicked the button")
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickHandler}>Click me</button>
+            </div>
+        );
+    }
+}
+
+export default ClassClick;
